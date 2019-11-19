b68c25d2f20ea382fc00b9d88bc3e0ba1f750ce3
daichi2673 <57936344+daichi2673@users.noreply.github.com>
2019-11-19 16:44:36 +0900

    Create confirm.php

---
+++ confirm.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daichi's Portfolio</title>
+    <link rel="stylesheet" href="./style.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="./script.js"></script>
+</head>
+        <!-- レスポンシブナビ -->
+        <header>
+            <div class="logo">Daichi's Portfolio</div>
+            <a class="nav-button" href="#">
+                <span class="bar-top"></span>
+                <span class="bar-middle"></span>
+                <span class="bar-bottom"></span>
+            </a>
+            <nav class="nav-wrap">
+                <ul class="sp-nav">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="profile.html">Profile</a></li>
+                    <li><a href="work.html">Work</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="container">
+            <div class="sidebar">
+                <h1>Daichi's Portfolio</h1>
+                <nav>
+                    <ul>
+                        <li class="nav-list"><a class="home-nav" href="index.html">Home</a></li>
+                        <li class="nav-list"><a class="profile-nav" href="Profile.html">Profile</a></li>
+                        <li class="nav-list"><a class="work-nav" href="work.html">Work</a></li>
+                        <li class="off-nav">Contact</li>
+                    </ul>
+                </nav>
+            </div>
+
+            <!-- ページの内容 -->
+            <div class="about">
+                <ul>
+                    <li class="nav-cover home-cover"><img src="./images/home_cover.jpeg"></li>
+                    <li class="nav-cover profile-cover"><img src="./images/Profile_cover.jpeg"></li>
+                    <li class="nav-cover work-cover"><img src="./images/Work_cover.jpeg"></li>
+                </ul>
+                <div class="inner contact-check">
+                    <h2>問合せ内容確認</h2>
+
+                    <form action="mailto.php" method="POST">
+                        <div class="check-items">
+                            <label>名前</label>
+                            <p><?php echo $_POST["name"]; ?></p>
+                        </div>
+                        <div class="check-items">
+                            <label>メールアドレス</label>
+                            <p><?php echo $_POST["mail"] ?></p>
+                        </div>
+                        <div class="check-items">
+                            <label>メッセージ</label>
+                            <p><?php echo $_POST["massage"] ?></p>
+                        </div>
+
+                        <input type="submit" value="送信"><a href="./contact.html">戻る</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
